67920d724f6e42561f552c231b3d305ae6052634
unknown <test@w3schools.com>
2023-03-13 18:56:13 +0900

    file systme

--- main.js
+++ main.js
@@ -1,36 +1,15 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+var fs = require("fs");
 
-// listener #1
-var listner1 = function listner1() {
-  console.log('listner1 executed.');
-}
+// Asynchronous read
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("Asynchronous read: " + data.toString());
+});
 
-// listener #2
-var listner2 = function listner2() {
-  console.log('listner2 executed.');
-}
+// Synchronous read
+var data = fs.readFileSync('input.txt');
+console.log("Synchronous read: " + data.toString());
 
-// Bind the connection event with the listner1 function
-eventEmitter.addListener('connection', listner1);
-
-// Bind the connection event with the listner2 function
-eventEmitter.on('connection', listner2);
-
-var eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-// Remove the binding of listner1 function
-eventEmitter.removeListener('connection', listner1);
-console.log("Listner1 will not listen now.");
-
-// Fire the connection event 
-eventEmitter.emit('connection');
-
-eventListeners = require('events').EventEmitter.listenerCount(eventEmitter, 'connection');
-console.log(eventListeners + " Listner(s) listening to connection event");
-
-console.log("Program Ended.");
+console.log("Program Ended");
